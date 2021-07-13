81505bbf8cc339f135e03cc04604fd897a34942c
Tyge Davis <53713175+tygedavis@users.noreply.github.com>
2021-07-12 20:47:13 -0600

    Credits page (#5)
    * Added a Credits page for any assets used by the website
    
    * Added a Credits page for any assets used by the website
    
    Co-authored-by: tygedavis <tdavis@cmsonline.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //Components
 import Home from './Pages/Home';
+import Credits from './Pages/Credits';
 import PageNotFound from './Pages/PageNotFound';
 import LinkShelf from './components/NavigationShelf';
 
@@ -15,6 +16,9 @@ function App() {
           <Switch>
               <Route exact path="/">
                   <Home/>
+              </Route>
+              <Route path="/credits">
+                  <Credits/>
               </Route>
               <Route path="*">
                   <PageNotFound />
